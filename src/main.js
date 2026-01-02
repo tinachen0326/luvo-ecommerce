3cc3ec3ad095557cc32fb121c398247dc8b58ba3
Rita <s5199676@gmail.com>
2026-01-02 15:58:22 +0800

    引入BaseInput套件

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import BaseCard from "@/components/base/BaseCard.vue";
 import BaseModal from "@/components/base/BaseModal.vue";
 import BasePagination from "@/components/base/BasePagination.vue";
 import BaseButton from "@/components/base/BaseButton.vue";
+import BaseInput from "@/components/base/BaseInput.vue";
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -14,7 +15,8 @@ const pinia = createPinia();
 app.component("BaseCard", BaseCard);
 app.component("BaseModal", BaseModal);
 app.component("BasePagination", BasePagination);
-app.component("BaseButton" , BaseButton)
+app.component("BaseButton" , BaseButton);
+app.component("BaseInput" , BaseInput);
 
 app.use(pinia);
 app.use(router);
