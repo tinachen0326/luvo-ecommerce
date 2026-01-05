f3266babbd092843a32920ec4619e34a689df54a
Rita <s5199676@gmail.com>
2026-01-05 10:03:57 +0800

    更改NewMenber.vue視覺設計

--- src/views/NewMember.vue
+++ src/views/NewMember.vue
@@ -32,7 +32,7 @@
     </section>
 
     <!-- 優惠說明 -->
-    <section class="bg-gradient-to-b from-blue-50 to-white py-12">
+    <section class="bg-gradient-to-b py-12">
       <div class="container mx-auto px-4">
         <div class="max-w-4xl mx-auto">
           <h2 class="text-3xl font-bold text-center mb-8 text-gray-800">
